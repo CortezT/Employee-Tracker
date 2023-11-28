85dc5cafa661df7863f6b9bb0efcfbb1a068a741
Trystan Cortez <trysyanmc22@gmail.com>
2023-11-28 13:12:08 -0600

    Added more functionality to user input

--- db/schema.sql
+++ db/schema.sql
@@ -3,23 +3,23 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-CREATE TABLE IF NOT EXISTS department (
+CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS role (
+CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL(10, 2),
+  title VARCHAR(255) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE IF NOT EXISTS employee (
+CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
   role_id INT,
   manager_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
